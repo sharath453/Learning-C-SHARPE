9fc89a3d45a203f5a5098ed8dbc14e3d5fccd5eb
sharath <sharath.i@99games.in>
2025-03-24 10:39:03 +0530

    Assignment3

---
+++ Assignment3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Timer = System.Timers.Timer;
+
+class Program
+{
+    static int remainingTime;
+    static int elapsedTime;
+    static Timer? timer;
+
+    static void Main()
+    {
+        Console.Write("Enter number of seconds: ");
+        remainingTime = int.Parse(Console.ReadLine() ?? "0");
+        elapsedTime = 0;
+        timer = new Timer(1000);
+        timer.Elapsed += Timer_Elapsed;
+        timer.Start();
+        Console.ReadLine();
+    }
+
+    static void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        elapsedTime++;
+        if (remainingTime > 0)
+        {
+            Console.WriteLine($"Time left: {remainingTime--} seconds, Elapsed time: {elapsedTime} seconds");
+        }
+        else
+        {
+            Console.WriteLine($"Time's up! Total elapsed time: {elapsedTime} seconds");
+            timer?.Stop();
+        }
+    }
+}
